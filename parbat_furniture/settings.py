6c4c5e91780af225e36fe45a3eb2cbd038496f17
Anilbk777 <anilbk775@gmail.com>
2025-07-28 17:31:18 +0545

    6

--- parbat_furniture/settings.py
+++ parbat_furniture/settings.py
@@ -45,17 +45,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'parbat_furniture.urls'
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'core',  # Add your core app
-]
+ROOT_URLCONF = 'parbat_furniture.urls'
 
 # Add template directory
 TEMPLATES = [
@@ -73,6 +64,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 # Static files configuration
 STATIC_URL = '/static/'
@@ -129,10 +121,9 @@ USE_I18N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
